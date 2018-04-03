095429f3b68deeed06c63808888ce6f53560ad9d
Louis Center <git@louis.center>
2018-04-03 21:57:07 +0200

    Added API data

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var choo = require('choo')
 var app = choo()
 
 app.use(require('./state/observations'))
+app.use(require('./state/satellites'))
 app.use(require('./state/stations'))
 
 app.route('/', require('./templates/example'))
--- state/stations.js
+++ state/stations.js
@@ -1,10 +1,37 @@
+var xhr = require('xhr')
+
 module.exports = stations
 
 function stations (state, emitter) {
-  state.stations = []
+  state.stations = {}
 
-  emitter.on('station:add', function (station) {
-    state.stations.push(station)
-    emitter.emit('render')
-  })
+  state.stations.byId = {}
+  state.stations.ordered = []
+
+  var pageCounter = 1
+  queryStations()
+
+  function queryStations () {
+    var opts = {
+      uri: `https://network.satnogs.org/api/stations/?page=${pageCounter}&format=json`,
+      json: true,
+      timeout: 10000
+    }
+
+    xhr(opts, function (err, res, body) {
+      if (err) return console.log('error', err)
+
+      body.forEach(function (station, i) {
+        var {id} = station
+
+        state.stations.byId[id] = station
+        state.stations.ordered.push(station)
+
+        if ((i + 1) === body.length && body.length === 25) {
+          pageCounter++
+          queryStations()
+        }
+      })
+    })
+  }
 }
