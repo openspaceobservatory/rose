15388ab81750f7c34f35632e250818f4f5c6e4be
Cory Levinson <cjlevinson@gmail.com>
2018-04-09 20:12:19 +0200

    first commit, adding recent obs/stations/satellites & trying to display w/ d3

--- screen-1.js
+++ screen-1.js
@@ -18,7 +18,34 @@ api(function () {
   var d = new Date()
   updateBgColor(d, window.state.weather.sunrise, window.state.weather.sunset)
 
+  var recentObs = Array.from(window.state.observations).sort(function(a, b) {
+    return Date.parse(a.end) <= Date.parse(b.end)
+  }).slice(0,5)
+
+  window.state.recentSats = recentObs.map(function(obs) {
+    //map to satellite id
+    return window.state.satellites.find( x => x.norad_cat_id == obs.norad_cat_id)
+  }).filter(function (v, i, self) {
+    // filter for unique values
+    return self.indexOf(v) === i
+  })
+
+  window.state.recentStations = recentObs.map(function(obs) {
+    //map to ground_stations
+    return window.state.stations.find( x => x.id == obs.ground_station)
+  }).filter(function (v, i, self) {
+    // filter for unique values
+    return self.indexOf(v) === i
+  })
+
   // update d3!
+  d3.select(".box-sats")
+    .selectAll(".box-sat")
+    .data(window.state.recentSats)
+    .enter()
+    .append("div")
+    .attr("class", "box-sat")
+
 })
 
 // initialise d3
