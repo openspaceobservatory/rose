e3efd9353d6de526eb1b7e931105ef17aec9ecfa
Louis Center <git@louis.center>
2018-04-09 19:57:02 +0200

    [WIP] screen 3 cycling images

--- screen-3.js
+++ screen-3.js
@@ -5,6 +5,7 @@ var isImage = require('./lib/is-image')
 
 window.state = {
   observations: [],
+  observationImages: [],
   satellites: [],
   stations: []
 }
@@ -17,43 +18,48 @@ var w = window,
     x = w.innerWidth || e.clientWidth || g.clientWidth,
     y = w.innerHeight|| e.clientHeight|| g.clientHeight;
 
-// initialise d3
-var svgContainer = d3.select("#content")
-                     .append("svg")
-                     .attr("class", "battleship")
-                     .attr("width", x)
-                     .attr("height", y)
-
-svgContainer.append('svg:image')
-  .attr('x', 0)
-  .attr('y', 0)
-  .attr('width', '75%')
-  .attr('height', '75%')
-
 var apiCounter = 0
+var imageIndex = 0
+var renderFlag = false
+
+var img = d.createElement('img')
+d.getElementById('content').appendChild(img)
+
 api(function () {
   // don't touch
   apiCounter++
   if (apiCounter < 3) return
 
-  // update d3!
-  svgContainer.select('image')
-    .data(window.state.observations)
-    .filter(function (d, i) {
-      return i === 0
-    })
-    .attr('xlink:href', function (d) {
-      if (d.demoddata.length > 1) {
-        var entry = d.demoddata[0]
-        var ext = fileExtension(entry)
+  var observations = w.state.observations
+  observations.forEach(function (observation, i) {
+    if (observation.demoddata.length > 1) {
+      var entry = observation.demoddata[0]
+      var ext = fileExtension(entry)
 
-        if (isImage(ext)) {
-          return entry
-        } else {
-          return d.waterfall
-        }
+      if (isImage(ext)) {
+        w.state.observationImages.push(entry)
       } else {
-        return d.waterfall
+        w.state.observationImages.push(observation.waterfall)
       }
-    })
+    } else {
+      w.state.observationImages.push(observation.waterfall)
+    }
+
+    if (i + 1 === observations.length) {
+      if (!renderFlag) {
+        renderFlag = true
+        render()
+      }
+    }
+  })
 })
+
+function render () {
+  renderLogic()
+  setInterval(renderLogic, 10000)
+}
+
+function renderLogic () {
+  img.src = w.state.observationImages[imageIndex]
+  imageIndex++
+}
