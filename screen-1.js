c2134bdc7ee7c088256b153013ebded837186e5c
Cory Levinson <cjlevinson@gmail.com>
2018-04-10 14:16:50 +0200

    adding carousel & prototype for screen-1

--- screen-1.js
+++ screen-1.js
@@ -1,5 +1,6 @@
 var api = require('./lib/api')
 var updateBgColor = require('./lib/background-color')
+var carousel = require('./lib/carousel')
 
 window.state = {
   observations: [],
@@ -13,8 +14,36 @@ window.state = {
 
 var apiCounter = 0
 
+function randomSatSprite() {
+  var satSprites = ["assets/dist/img/cubesat1.png", "assets/dist/img/cubesat2.png"]
+  return satSprites[Math.round(Math.random())]
+}
+
 api(function () {
+  // update background color
+  var d = new Date()
+  updateBgColor(d, window.state.weather.sunrise, window.state.weather.sunset)
+
+  var carouselSats = carousel.satellites()
+
   // update d3!
+  var boxSat = d3.select(".box-sats")
+                 .selectAll(".box-sat")
+                 .data(carouselSats)
+                 .enter()
+                 .append("div")
+                 .attr("class", "box-sat")
+
+  boxSat.append("span")
+        .attr("class", "name-sat")
+        .html(d => d.name)
+  boxSat.append("br")
+  boxSat.append("span")
+        .attr("class", "description-icon")
+        .html("Origin Information")
+  boxSat.append("img")
+        .attr("src", randomSatSprite())
+
 })
 
 // initialise d3
